fee8226844c668c84e8ac955ff2157e2071c6129
MdSajjadHossin <iamsajjadhossin@gmail.com>
2022-07-29 23:55:31 +0600

    single message alert dialog added

---
+++ app/src/main/java/com/example/alertdialogandroid/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.alertdialogandroid;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Single Button Alert Dialog
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
+        alertDialog.setTitle("Terms & Condition");
+        alertDialog.setIcon(R.drawable.ic_baseline_perm_device_information_24);
+        alertDialog.setMessage("Are Agree With All the Terms & Condition!");
+
+        alertDialog.setButton("Continue", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Yes", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
