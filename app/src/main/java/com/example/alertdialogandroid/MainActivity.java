a82f17e47a0a8214433fc9a81a1e50d9bd143b3c
MdSajjadHossin <iamsajjadhossin@gmail.com>
2022-08-01 16:04:40 +0600

    exit on back press dialog box added

--- app/src/main/java/com/example/alertdialogandroid/MainActivity.java
+++ app/src/main/java/com/example/alertdialogandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.alertdialogandroid;
 
+import androidx.activity.OnBackPressedDispatcherOwner;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,21 +19,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Single Button Alert Dialog
-        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+//        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+//
+//        alertDialog.setTitle("Terms & Condition");
+//        alertDialog.setIcon(R.drawable.ic_baseline_perm_device_information_24);
+//        alertDialog.setMessage("Are Agree With All the Terms & Condition!");
+//
+//        alertDialog.setButton(Dialog.BUTTON_POSITIVE, "Ok", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                Toast.makeText(MainActivity.this, "Yes", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//       alertDialog.show();
 
-        alertDialog.setTitle("Terms & Condition");
-        alertDialog.setIcon(R.drawable.ic_baseline_perm_device_information_24);
-        alertDialog.setMessage("Are Agree With All the Terms & Condition!");
-
-        alertDialog.setButton(Dialog.BUTTON_POSITIVE, "Ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(MainActivity.this, "Yes", Toast.LENGTH_SHORT).show();
-            }
-        });
-       alertDialog.show();
-
-       //Two Button Alert Dialog
+        //Two Button Alert Dialog
         AlertDialog.Builder deleteDialog = new AlertDialog.Builder(MainActivity.this);
         deleteDialog.setTitle("Want To Delete?");
         deleteDialog.setIcon(R.drawable.ic_baseline_delete_24);
@@ -54,5 +55,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         deleteDialog.show();
+
+    }
+        //Three Button Alert Dialog
+
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder exitDialog = new AlertDialog.Builder(this);
+
+        exitDialog.setIcon(R.drawable.ic_baseline_exit_to_app_24);
+        exitDialog.setTitle("Exit?");
+        exitDialog.setMessage("Are You Sure You Want to Exit");
+        exitDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Welcome Back", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        exitDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MainActivity.super.onBackPressed();
+                Toast.makeText(MainActivity.this, "Good Bye", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        exitDialog.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Activity Canceled", Toast.LENGTH_SHORT).show();
+            }
+        });
+        exitDialog.show();
     }
 }
