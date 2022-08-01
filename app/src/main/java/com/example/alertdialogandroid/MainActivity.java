c1473a58b23b728e07be549e19e53e2eb91d8f04
MdSajjadHossin <iamsajjadhossin@gmail.com>
2022-08-01 15:05:07 +0600

    Single option alert dialog error fixed

--- app/src/main/java/com/example/alertdialogandroid/MainActivity.java
+++ app/src/main/java/com/example/alertdialogandroid/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.alertdialogandroid;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -22,11 +24,12 @@ public class MainActivity extends AppCompatActivity {
         alertDialog.setIcon(R.drawable.ic_baseline_perm_device_information_24);
         alertDialog.setMessage("Are Agree With All the Terms & Condition!");
 
-        alertDialog.setButton("Continue", new DialogInterface.OnClickListener() {
+        alertDialog.setButton(Dialog.BUTTON_POSITIVE, "Ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Toast.makeText(MainActivity.this, "Yes", Toast.LENGTH_SHORT).show();
             }
         });
+       alertDialog.show();
     }
 }
